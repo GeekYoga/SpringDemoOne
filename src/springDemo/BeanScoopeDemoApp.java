562a418d663472ea9daa39b29743a2bc29c4bc52
anastasia <anaspa@ttu.ee>
2021-06-11 15:11:13 +0300

    bean scopes: prototype

---
+++ src/springDemo/BeanScoopeDemoApp.java
@@ -0,0 +1,24 @@
+package springDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScoopeDemoApp {
+
+    public static void main(String[] args) {
+
+        // Load the spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanScope_applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach coach = context.getBean("myCoach", Coach.class);
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        // print out results
+        System.out.println("Memory location for coach: " + coach);
+        System.out.println("Memory location for alphaCoach: " + alphaCoach);
+
+        // close the context
+        context.close();
+    }
+}
